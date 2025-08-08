3e7f0ca3e8f287bc5389a4be067dc2948ff666a2
huyhieublock <huy.hieu@blockodyssey.io>
2025-08-08 10:57:58 +0700

    frist commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Hero from "./sections/Hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
+
+const App = () => {
+  return (
+    <>
+      <Hero />
+      <ShowcaseSection />
+    </>
+  );
+};
+
+export default App;
